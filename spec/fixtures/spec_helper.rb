c80b00923318b3b0b532705fe495cb07b5227bb1
Fifi <fifi.ding@iss.nthu.edu.tw>
2024-10-13 23:13:41 +0800

    rebase

---
+++ spec/fixtures/spec_helper.rb
@@ -0,0 +1,17 @@
+# Contains libraries and constants 
+require 'simplecov'
+SimpleCov.start
+
+
+require 'minitest/autorun'
+require 'minitest/unit'
+require 'minitest/rg'
+require 'yaml'
+require_relative '../lib/playlist_processor'
+
+CONFIG = YAML.safe_load_file('config/secrets.yml')
+YOUTUBE_API_KEY = CONFIG['YOUTUBE_API_KEY']
+CORRECT = YAML.safe_load_file('spec/fixtures/playlist_results.yml')
+
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
+CASSETTE_FILE = 'check_YTapi_casette'
