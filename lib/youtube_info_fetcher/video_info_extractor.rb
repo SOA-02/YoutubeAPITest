d8b53954383b75c0618bd00f075d5beea7a62e68
Fifi <fifi.ding@iss.nthu.edu.tw>
2024-10-13 20:12:28 +0800

    add video_info_extractor

---
+++ lib/youtube_info_fetcher/video_info_extractor.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module YouTubeInfoFetcher
+  class VideoInfoExtractor
+    def extract(data)
+      return unless data && data['items'] && !data['items'].empty?
+
+      snippet = data['items'].first['snippet']
+      {
+        title: snippet['title'],
+        published_at: snippet['publishedAt'],
+        channel_title: snippet['channelTitle'],
+        description: snippet['description'],
+        thumbnail_url: snippet['thumbnails']['high']['url']
+      }
+    end
+  end
+end
