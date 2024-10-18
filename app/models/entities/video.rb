7ef541cffee5a68dc1a6860a5f324729851e9880
Fifi <fifi.ding@iss.nthu.edu.tw>
2024-10-18 12:08:27 +0800

    video_mapper and video entity

--- app/models/entities/video.rb
+++ app/models/entities/video.rb
@@ -1,51 +1,20 @@
 # frozen_string_literal: true
 
-# require_relative 'channel'
-# require_relative 'playlist'
-
-require 'dry-struct'
 require 'dry-types'
-require 'yaml'
-require_relative '../../../app/models/gateways/video_api'
+require 'dry-struct'
 
-config = YAML.load_file(File.expand_path('../../../config/secrets.yml', __dir__))
-
-# Define the Types module for Dry Types
-module Types
-  include Dry.Types()
-end
-
-# Video entity
 module Outline
   module Entity
-    # Video entity
+    # Domain entity for video
     class Video < Dry::Struct
       include Dry.Types
-      attribute :id, Types::Strict::String
-      attribute :title, Types::Strict::String
-      attribute :description, Types::Strict::String
-      attribute :published_at, Types::Strict::Time
-      attribute :thumbnail_url, Types::Strict::String
+
+      attribute? :video_id, Strict::String
+      attribute :video_title, Strict::String
+      attribute :video_published_at, Strict::Time
+      attribute :video_description, Strict::String
+      attribute :video_thumbnail_url, Strict::String
+      attribute :video_tags, Strict::Array.of(Strict::String).optional
     end
   end
 end
-
-# Example Usage
-example_response = Outline::Entity::Video.new(
-  id: '1234567890',
-  title: 'Sample Video Title',
-  description: 'This is a sample video description.',
-  published_at: Time.now,
-  thumbnail_url: 'https://example.com/thumbnail.jpg'
-)
-
-example_video = Outline::Entity::Video.new(example_response.to_h)
-puts example_video.id
-
-# Actual Usage
-api_key = config['API_KEY']
-video_id = 'jeqH4eMGjhY'
-
-api_response = Outline::YoutubeApi.new(api_key)
-video = Outline::Entity::Video.new(api_response)
-puts video.id
--- app/models/mappers/video_mapper.rb
+++ app/models/mappers/video_mapper.rb
@@ -1,60 +1,70 @@
 # frozen_string_literal: true
 
+require 'yaml'
+require_relative '../gateways/youtube_api'
+require_relative '../entities/video'
+
 module Outline
   module Youtube
     # Video mapper: Youtube repo -> Video entity
     class VideoMapper
-      def initialize(yt_key, gateway_class)
+      def initialize(yt_key, gateway_class = Outline::Youtube::YoutubeApi)
         @api_key = yt_key
         @gateway_class = gateway_class
         @gateway = gateway_class.new(@api_key)
       end
 
-      def find(id, title)
-        data = @gateway.video_data(id, title)
-        build_entity(data)
+      def find(video_id)
+        video_data = @gateway.video_info(video_id)
+        item_data = video_data['items'].first
+        build_entity(item_data)
       end
 
-      def build_entity(data)
-        DataMapper.new(data, @token, @gateway_class).build_entity
+      def build_entity(items_data)
+        DataMapper.new(items_data).build_entity
       end
 
       # Extracts entity specific elements from data structure
       class DataMapper
-        def initialize(data, token, gateway_class)
-          @data = data
-          @token = token
-          @gateway_class = gateway_class
+        def initialize(items_data)
+          @items_data = items_data['snippet']
         end
 
         def build_entity
           Outline::Entity::Video.new(
-            title: title,
-            published_at: publishedAt,
-            description: description,
-            thumbnail_url: thumbnail_url,
-            tags: tags
+            video_id: id,
+            video_title: title,
+            video_published_at: published_at,
+            video_description: description,
+            video_thumbnail_url: thumbnail_url,
+            video_tags: tags
           )
         end
 
+        private
+
+        def id
+          @items_data['id']
+        end
+
         def title
-          @video_info['title']
+          @items_data['title']
         end
 
         def published_at
-          @video_info['publishedAt']
+          @items_data['publishedAt']
         end
 
         def description
-          @video_info['description']
+          @items_data['description']
         end
 
         def thumbnail_url
-          @video_info['thumbnails']['high']['url']
+          @items_data['thumbnails']['high']['url']
         end
 
         def tags
-          @video_info['tags']
+          @items_data['tags']
         end
       end
     end
