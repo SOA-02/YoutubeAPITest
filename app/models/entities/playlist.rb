62eb81e7e61ec0b7ffd4f95855689007771bf121
root <root@LAPTOP-8RON5KJV>
2024-10-21 22:58:06 +0800

    fix: fix my stupid rubocop issue in playlist.rb :P

--- app/models/entities/playlist.rb
+++ app/models/entities/playlist.rb
@@ -2,13 +2,14 @@
 
 require 'dry-types'
 require 'dry-struct'
+require 'rubocop-minitest'
+require 'rubocop-rake'
 
 module Outline
-  module Entity 
+  module Entity
     # domain entity for playlist
     class Playlist < Dry::Struct
       include Dry.types
-
       attribute :playlist_id, Strict::String
       attribute? :playlist_title, Strict::String
       attribute? :playlist_published_at, Strict::Time
@@ -17,13 +18,6 @@ module Outline
       attribute? :playlist_item_count, Strict::Integer
       # attribute? :playlist_item_id, Strict::list
       # attribute? :playlist_item_title, Strict::list
-
     end
-
-
-
-
-
-
-
-
+  end
+end
