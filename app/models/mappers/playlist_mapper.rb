b9f2fd9de0638383bb5d9224c1ef2bd71a3583a0
root <root@LAPTOP-8RON5KJV>
2024-10-21 23:15:16 +0800

    fix: fix stupid rubocop issuess in playlist_mapper.rb :P

--- app/models/mappers/playlist_mapper.rb
+++ app/models/mappers/playlist_mapper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Outline
-  module Youtube 
-    class PlaylistMapper
+  module Youtube
+    class Playlist
       def initialize(api_key, gateway_class = YouTubeApi)
         @api_key = api_key
         @gateway_class = gateway_class
         @gateway = gateway_class.new(@api_key)
-      end 
+      end
 
       def find(playlist_id)
         playlist_info = @gateway.playlist_info(playlist_id)
@@ -15,59 +15,56 @@ module Outline
         raise 'Playlist data is missing' unless item_data
 
         build_entity(item_data)
-      end 
+      end
 
       def build_entity(data)
         Datamapper.new(data).build_entity
-      end 
+      end
 
-      class Datamapper 
+      # Maps YouTube data to Playlist entities.
+      class Datamapper
         def initialize(data)
           @data = data['snippet']
-          raise 'Snippet data missing' unless data
-        end 
+          raise 'Snippet data missing' unless @data
+        end
 
-      def build_entity
-        Entity::Playlist.new(
-          playlist_id: id,
-          playlist_title: title, 
-          playlist_published_at: published_at, 
-          playlist_description: description, 
-          playlist_thumbnail_url: thumbnail_url, 
-          playlist_item_count: item_count, 
-        )
-      end 
+        def build_entity
+          Entity::Playlist.new(
+            playlist_id: id,
+            playlist_title: title,
+            playlist_published_at: published_at,
+            playlist_description: description,
+            playlist_thumbnail_url: thumbnail_url,
+            playlist_item_count: item_count
+          )
+        end
 
-      private 
-      def id
-        @items_data['id']
-      end
+        private
 
-      def title
-        @items_data['title']
-      end
+        def id
+          @data['id']
+        end
 
-      def published_at
-        @items_data['publishedAt']
-      end
+        def title
+          @data['title']
+        end
 
-      def description
-        @items_data['description']
-      end
+        def published_at
+          @data['publishedAt']
+        end
 
-      def thumbnail_url
-        @items_data['thumbnails']['high']['url']
-      end
+        def description
+          @data['description']
+        end
 
-      # def item_count
-      #   @items_data['itemCount']
+        def thumbnail_url
+          @data['thumbnails']&.dig('high', 'url')
+        end
+
+        def item_count
+          @data['itemCount']
+        end
       end
     end
   end
 end
-end
-
-
-
-
-
