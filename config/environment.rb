c80eec1de284bfb0a55e474c358aaaa9f839a74c
root <root@LAPTOP-8RON5KJV>
2024-10-24 13:41:36 +0800

    fix: fix environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,26 @@
 
 require 'roda'
 require 'yaml'
+require 'figaro'
 
 module Outline
   # Configuration for the App
   class App < Roda
     CONFIG = YAML.safe_load_file('config/secrets.yml')
     API_KEY = CONFIG['API_KEY']
+  end
+end
+
+module Outline
+  class Api < Roda
+    plugin :environments
+
+    Figaro.application = Figaro::Application.new(
+      environment: environment,
+      path: File.expand_path('config/secrets.yml')
+    )
+    Figaro.load
+
+    def self.config = Figaro.env
   end
 end
