88c41c4c18ac32bcca436c3c7b0200e14b8e7947
Fifi <fifi.ding@iss.nthu.edu.tw>
2024-10-24 19:47:19 +0800

    update video orm

--- app/infrastructure/database/video_orm.rb
+++ app/infrastructure/database/video_orm.rb
@@ -3,8 +3,8 @@
 module Outline
   module Database
     # Object Relational Mapper for Video Entities
-    class VideoOrm < Sequel::Model(:projects)
-      many_to_one :id
+    class VideoOrm < Sequel::Model(:videos)
+      many_to_one :channel, class: :'Outline::Database::ChannelOrm', key: :channel_id
 
       plugin :timestamps, update_on_create: true
     end
