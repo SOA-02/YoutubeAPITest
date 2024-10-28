3dc68d24171adfe37c75e84d11c49b8423e1c08d
root <root@LAPTOP-8RON5KJV>
2024-10-28 11:37:14 +0800

    fix: fix spec_helper conflict

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,18 +15,7 @@ require 'webmock'
 require_relative '../require_app'
 require_app
 
-# 載入 secrets.yml 設定
-CONFIG = YAML.safe_load_file(File.expand_path('../config/secrets.yml', __dir__))
-
-# 根據執行環境 (development, test, production) 取得對應的 API_KEY
-environment = ENV['RACK_ENV'] || 'development' # 預設為 development
-API_KEY = CONFIG[environment]['API_KEY']
-
-# 輸出檢查是否正確載入 API_KEY
-puts "Loaded API_KEY: #{API_KEY}"
-
-CASSETTES_FOLDER = 'spec/fixtures/cassettes'
-# CASSETTE_FILE = 'youtube_api'
+CHANNEL_ID = 'UCpYf6C9QsP_BRf97vLuXlIA'
 VIDEO_ID = 'GZe2FJOWGIs'
 API_KEY = Outline::App.config.API_KEY
 CORRECT = YAML.safe_load_file('spec/fixtures/youtube_channel_info.yml')
