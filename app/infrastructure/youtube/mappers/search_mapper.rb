31e33ca8c917ec63822589fe5fc8001d2a2e172d
MING-CHUNLee <mindy80230@gmail.com>
2024-10-28 20:40:49 +0800

    fix: Filter search results to include only video items

--- app/infrastructure/youtube/mappers/search_mapper.rb
+++ app/infrastructure/youtube/mappers/search_mapper.rb
@@ -21,17 +21,19 @@ module Outline
         data = @gateway.search_info(key_word)
         # 確認 `items` 是否有內容，並取得第一個項目
         item_data = data['items']
-        puts "Channel Data: #{item_data}"
-        raise 'Video data is missing' unless item_data # 若資料為空則報錯
+        puts "Item data: #{item_data}"
+        raise 'Video data is missing' if item_data.nil? || item_data.empty? # 若資料為空則報錯
 
-        parsed_items =  item_data.map do |item|
-          puts "=== Item: #{item.inspect}"
+        # 只選擇 id.kind 為 'youtube#video' 的項目
+        video_items = item_data.select { |item| item['id']['kind'] == 'youtube#video' }
+        raise 'No video items found' if video_items.empty? # 若沒有視頻項目則報錯
+
+        video_items.map do |item|
           build_entity(item)
-        end
-        # 回傳所有解析後的項目
-        parsed_items
-      end
 
+        end
+      end
+      
       def build_entity(data)
         puts "=== data===: #{data.inspect}"
         DataMapper.new(data).build_entity
@@ -44,7 +46,6 @@ module Outline
           raise 'Snippet data missing' unless @data # 若無 `snippet` 則報錯
         end
 
-        # rubocop:disable Metrics/MethodLength
         def build_entity
           Entity::Search.new(
             id: nil,
@@ -55,7 +56,6 @@ module Outline
             default_thumbnail_url:
           )
         end
-        # rubocop:enable Metrics/MethodLength
 
         private
 
