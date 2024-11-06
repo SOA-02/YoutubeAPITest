2e6c419c218e529200770f67b34995b4023f93f6
MING-CHUNLee <mindy80230@gmail.com>
2024-11-07 00:23:13 +0800

    FIX:undefined method logger

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -111,7 +111,7 @@ module Outline
               video = Youtube::VideoMapper
                 .new(App.config.API_KEY).find(video_id)
             rescue StandardError => err
-              App.logger.error err.backtrace.join("DB READ PROJ\n")
+
               flash[:error] = MSG_VID_NOT_FOUND
               response.status = 404
               routing.redirect '/'
